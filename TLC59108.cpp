774e99456c706d83ad8d20fc498c39257bd17c31
Wenzel Reichmuth <wenzel.reichmuth@vraktion.com>
2021-10-15 16:25:34 +0200

    210115

--- TLC59108.cpp
+++ TLC59108.cpp
@@ -1,87 +1,87 @@
-    #include "TLC59108.h"
-    
-    TLC59108::TLC59108(PinName p_sda, PinName p_scl, const uint8_t selectable_address):
-        addr(I2C_ADDR::BASE | selectable_address), i2c(new I2C(p_sda, p_scl))
-    {
-    }
+#include "TLC59108.h"
 
-    uint8_t TLC59108::init(){
-        reset();
-        // wait_ms(200);
-        ThisThread::sleep_for(200);
-        uint8_t status = 0;
-        status &= this->setRegister(TLC59108::REGISTER::MODE1::ADDR, TLC59108::REGISTER::MODE1::ALLCALL);
-        status &= this->setLedOutputMode(TLC59108::LED_MODE::PWM_IND);//PWM_INDGRP);//
-        status &= setBrightness((const uint8_t ) 0);
-        return status;
-    }
+TLC59108::TLC59108(PinName p_sda, PinName p_scl,
+                   const uint8_t selectable_address)
+    : addr(I2C_ADDR::BASE | selectable_address), i2c(new I2C(p_sda, p_scl)) {}
 
-    uint8_t TLC59108::reset(){
-        setLedOutputMode(LED_MODE::OFF);
-        setRegister(REGISTER::MODE1::ADDR, REGISTER::MODE1::OSC_OFF);
-        uint8_t status = 0;
-        this->i2c->start();
-        status &= this->i2c->write(0x96);
-        status &= this->i2c->write(0xA5);
-        status &= this->i2c->write(0x5A);
-        this->i2c->stop();
-        return status;
-    }
+uint8_t TLC59108::init() {
+  reset();
+  wait_us(200000);
+  // ThisThread::sleep_for(200);
+  uint8_t status = 0;
+  status &= this->setRegister(TLC59108::REGISTER::MODE1::ADDR,
+                              TLC59108::REGISTER::MODE1::ALLCALL);
+  status &=
+      this->setLedOutputMode(TLC59108::LED_MODE::PWM_IND); // PWM_INDGRP);//
+  status &= setBrightness((const uint8_t)0);
+  return status;
+}
 
-    uint8_t TLC59108::setLedOutputMode(const uint8_t outputMode)
-    {
-        if(outputMode & 0xfc)
-            return 0;
-        
-        uint8_t regValue = (outputMode << 6) | (outputMode << 4) | (outputMode << 2) | outputMode;
-        
-        uint8_t retVal = setRegister(TLC59108::REGISTER::LEDOUT0::ADDR, regValue);
-        retVal &= setRegister(TLC59108::REGISTER::LEDOUT1::ADDR, regValue);
-        return retVal;
-    }
+uint8_t TLC59108::reset() {
+  setLedOutputMode(LED_MODE::OFF);
+  setRegister(REGISTER::MODE1::ADDR, REGISTER::MODE1::OSC_OFF);
+  uint8_t status = 0;
+  this->i2c->start();
+  status &= this->i2c->write(0x96);
+  status &= this->i2c->write(0xA5);
+  status &= this->i2c->write(0x5A);
+  this->i2c->stop();
+  return status;
+}
 
-    uint8_t TLC59108::setBrightness(const uint8_t pwmChannel, const uint8_t dutyCycle)
-    {
-        if(pwmChannel > 7)
-            return 0;//ERROR::EINVAL;
-        
-        return setRegister(TLC59108::REGISTER::PWM0::ADDR + pwmChannel, dutyCycle);
-    }
+uint8_t TLC59108::setLedOutputMode(const uint8_t outputMode) {
+  if (outputMode & 0xfc)
+    return 0;
 
-    uint8_t TLC59108::setBrightness(const uint8_t dutyCycle)
-    {
-        uint8_t status = 0;
-        this->i2c->start();
-        status &= this->i2c->write(addr);
-        status &= this->i2c->write(TLC59108::REGISTER::PWM0::ADDR | TLC59108::AUTO_INCREMENT::IND);
-        for (uint8_t i = 0; i < NUM_CHANNELS; i++)
-            status &= this->i2c->write(dutyCycle);
-        this->i2c->stop();
-        return status;
-    }
-    
-    uint8_t TLC59108::setBrightness(const uint8_t dutyCycles[], const uint8_t length) 
-    {
-        return setRegisters(TLC59108::REGISTER::PWM0::ADDR, dutyCycles, length);
-    }
+  uint8_t regValue =
+      (outputMode << 6) | (outputMode << 4) | (outputMode << 2) | outputMode;
 
-    uint8_t TLC59108::setGroupBrightness(const uint8_t dutyCycle)
-    {
-        return setRegister(TLC59108::REGISTER::GRPPWM::ADDR, dutyCycle);
-    }
+  uint8_t retVal = setRegister(TLC59108::REGISTER::LEDOUT0::ADDR, regValue);
+  retVal &= setRegister(TLC59108::REGISTER::LEDOUT1::ADDR, regValue);
+  return retVal;
+}
 
-    int TLC59108::setRegister(const uint8_t reg, const uint8_t value) {
-        char cmd[2] = { reg, value };
-        return this->i2c->write(addr, cmd, 2);
-    }
-    
-    int TLC59108::setRegisters(const uint8_t reg, const uint8_t values[], const uint8_t length) {
-        uint8_t status = 0;
-        this->i2c->start();
-        status &= this->i2c->write(addr);
-        status &= this->i2c->write(reg | TLC59108::AUTO_INCREMENT::ALL);
-        for (uint8_t i = 0; i < length; i++)
-            status &= this->i2c->write(values[i]);
-        this->i2c->stop();
-        return status;
-    }
+uint8_t TLC59108::setBrightness(const uint8_t pwmChannel,
+                                const uint8_t dutyCycle) {
+  if (pwmChannel > 7)
+    return 0; // ERROR::EINVAL;
+
+  return setRegister(TLC59108::REGISTER::PWM0::ADDR + pwmChannel, dutyCycle);
+}
+
+uint8_t TLC59108::setBrightness(const uint8_t dutyCycle) {
+  uint8_t dutyCycles[8] = {dutyCycle};
+  return this->setBrightness(dutyCycles);
+}
+
+uint8_t TLC59108::setBrightness(const uint8_t dutyCycles[],
+                                const uint8_t startChannel,
+                                const uint8_t length) {
+  if (startChannel + length <= NUM_CHANNELS) {
+    return setRegisters(TLC59108::REGISTER::PWM0::ADDR + startChannel,
+                        dutyCycles, length);
+  } else {
+    return 1;
+  }
+}
+
+uint8_t TLC59108::setGroupBrightness(const uint8_t dutyCycle) {
+  return setRegister(TLC59108::REGISTER::GRPPWM::ADDR, dutyCycle);
+}
+
+int TLC59108::setRegister(const uint8_t reg, const uint8_t value) {
+  char cmd[2] = {reg, value};
+  return this->i2c->write(addr, cmd, 2);
+}
+
+int TLC59108::setRegisters(const uint8_t reg, const uint8_t values[],
+                           const uint8_t length) {
+  uint8_t status = 0;
+
+  char *buff = (char *)malloc(length + 1);
+  *buff = reg | TLC59108::AUTO_INCREMENT::ALL;
+  memcpy(buff + 1, values, length);
+  status = this->i2c->write(addr, buff, length + 1, false);
+  free(buff);
+  return status;
+}
--- TLC59108.h
+++ TLC59108.h
@@ -1,8 +1,9 @@
 /*
- * TLC59108: Arduino library to control TI TLC59108/TLC59108F/TLC59208 LED drivers
- * 
+ * TLC59108: Arduino library to control TI TLC59108/TLC59108F/TLC59208 LED
+ * drivers
+ *
  * (C) 2013 Christopher Smith <chrylis@gmail.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -22,173 +23,155 @@
 
 #include "mbed.h"
 
-class TLC59108
-{
+class TLC59108 {
 public:
-    // default I2C addresses
-    // datasheet, pp 12-13
-    struct I2C_ADDR
-    {
-        static const uint8_t BASE = 0x80;
-        static const uint8_t SWRESET = 0x4b;
-        static const uint8_t ALLCALL = 0x90;
-        static const uint8_t SUB1 = 0x92;
-        static const uint8_t SUB2 = 0x94;
-        static const uint8_t SUB3 = 0x98;
+  // default I2C addresses
+  // datasheet, pp 12-13
+  struct I2C_ADDR {
+    static const uint8_t BASE = 0x80;
+    static const uint8_t SWRESET = 0x4b;
+    static const uint8_t ALLCALL = 0x90;
+    static const uint8_t SUB1 = 0x92;
+    static const uint8_t SUB2 = 0x94;
+    static const uint8_t SUB3 = 0x98;
+  };
+
+  // register auto-increment modes for setting multiple registers
+  // datasheet, p 13
+  struct AUTO_INCREMENT {
+    static const uint8_t ALL =
+        0x80; // increment through all registers (for initial setup)
+    static const uint8_t IND =
+        0xa0; // increment through individual brightness registers
+    static const uint8_t GLOBAL =
+        0xc0; // increment through global control registers
+    static const uint8_t INDGLOBAL =
+        0xe0; // increment through individual and global registers
+  };
+
+  struct LED_MODE {
+    static const uint8_t OFF = 0;
+    static const uint8_t FULL_ON = 1;
+    static const uint8_t PWM_IND = 2;
+    static const uint8_t PWM_INDGRP = 3;
+  };
+
+  // register names
+  // datasheet, p 16
+  struct REGISTER {
+  public:
+    struct MODE1 {
+      static const uint8_t ADDR = 0x00;
+
+      static const uint8_t OSC_OFF = 0x10;
+      static const uint8_t SUB1 = 0x08;
+      static const uint8_t SUB2 = 0x04;
+      static const uint8_t SUB3 = 0x02;
+      static const uint8_t ALLCALL = 0x01;
     };
 
-    // register auto-increment modes for setting multiple registers
-    // datasheet, p 13
-    struct AUTO_INCREMENT
-    {
-        static const uint8_t ALL = 0x80; // increment through all registers (for initial setup)
-        static const uint8_t IND = 0xa0; // increment through individual brightness registers
-        static const uint8_t GLOBAL = 0xc0; // increment through global control registers
-        static const uint8_t INDGLOBAL = 0xe0; // increment through individual and global registers
+    struct MODE2 {
+      static const uint8_t ADDR = 0x01;
+
+      static const uint8_t EFCLR = 0x80;
+      static const uint8_t DMBLNK = 0x20;
+      static const uint8_t OCH = 0x08;
     };
 
-    struct LED_MODE
-    {
-        static const uint8_t OFF = 0;
-        static const uint8_t FULL_ON = 1;
-        static const uint8_t PWM_IND = 2;
-        static const uint8_t PWM_INDGRP = 3;
+    struct PWM0 {
+      static const uint8_t ADDR = 0x02;
     };
 
-    // register names
-    // datasheet, p 16
-    struct REGISTER
-    {
-    public:
-        struct MODE1
-        {
-            static const uint8_t ADDR = 0x00;
-
-            static const uint8_t OSC_OFF = 0x10;
-            static const uint8_t SUB1 = 0x08;
-            static const uint8_t SUB2 = 0x04;
-            static const uint8_t SUB3 = 0x02;
-            static const uint8_t ALLCALL = 0x01;
-        };
-
-        struct MODE2
-        {
-            static const uint8_t ADDR = 0x01;
-
-            static const uint8_t EFCLR = 0x80;
-            static const uint8_t DMBLNK = 0x20;
-            static const uint8_t OCH = 0x08;
-        };
-
-        struct PWM0
-        {
-            static const uint8_t ADDR = 0x02;
-        };
-
-        struct PWM1
-        {
-            static const uint8_t ADDR = 0x03;
-        };
-
-        struct PWM2
-        {
-            static const uint8_t ADDR = 0x04;
-        };
-
-        struct PWM3
-        {
-            static const uint8_t ADDR = 0x05;
-        };
-
-        struct PWM4
-        {
-            static const uint8_t ADDR = 0x06;
-        };
-
-        struct PWM5
-        {
-            static const uint8_t ADDR = 0x07;
-        };
-
-        struct PWM6
-        {
-            static const uint8_t ADDR = 0x08;
-        };
-
-        struct PWM7
-        {
-            static const uint8_t ADDR = 0x09;
-        };
-
-        struct GRPPWM
-        {
-            static const uint8_t ADDR = 0x0a;
-        };
-
-        struct GRPFREQ
-        {
-            static const uint8_t ADDR = 0x0b;
-        };
-
-        struct LEDOUT0
-        {
-            static const uint8_t ADDR = 0x0c;
-        };
-
-        struct LEDOUT1
-        {
-            static const uint8_t ADDR = 0x0d;
-        };
-
-        struct SUBADR1
-        {
-            static const uint8_t ADDR = 0x0e;
-        };
-
-        struct SUBADR2
-        {
-            static const uint8_t ADDR = 0x0f;
-        };
-
-        struct SUBADR3
-        {
-            static const uint8_t ADDR = 0x10;
-        };
-
-        struct ALLCALLADR
-        {
-            static const uint8_t ADDR = 0x11;
-        };
-
-        struct IREF
-        {
-            static const uint8_t ADDR = 0x12;
-
-            static const uint8_t CM = 0x80; // current multiplier
-            static const uint8_t HC = 0x40; // subcurrent
-        };
-
-        struct EFLAG
-        {
-            static const uint8_t ADDR = 0x13;
-        };
+    struct PWM1 {
+      static const uint8_t ADDR = 0x03;
     };
 
-    TLC59108(PinName p_sda, PinName p_scl, const uint8_t selectable_address = 0);
-    uint8_t init();
-    uint8_t reset();
-    uint8_t setLedOutputMode(const uint8_t outputMode);
-    uint8_t setBrightness(const uint8_t pwmChannel, const uint8_t dutyCycle);
-    uint8_t setBrightness(const uint8_t dutyCycle);
-    uint8_t setBrightness(const uint8_t dutyCycles[], const uint8_t length = NUM_CHANNELS);
-    uint8_t setGroupBrightness(const uint8_t dutyCycle);
-    int setRegister(const uint8_t reg, const uint8_t value);
-    int setRegisters(const uint8_t reg, const uint8_t values[], const uint8_t length);
+    struct PWM2 {
+      static const uint8_t ADDR = 0x04;
+    };
+
+    struct PWM3 {
+      static const uint8_t ADDR = 0x05;
+    };
+
+    struct PWM4 {
+      static const uint8_t ADDR = 0x06;
+    };
+
+    struct PWM5 {
+      static const uint8_t ADDR = 0x07;
+    };
+
+    struct PWM6 {
+      static const uint8_t ADDR = 0x08;
+    };
+
+    struct PWM7 {
+      static const uint8_t ADDR = 0x09;
+    };
+
+    struct GRPPWM {
+      static const uint8_t ADDR = 0x0a;
+    };
+
+    struct GRPFREQ {
+      static const uint8_t ADDR = 0x0b;
+    };
+
+    struct LEDOUT0 {
+      static const uint8_t ADDR = 0x0c;
+    };
+
+    struct LEDOUT1 {
+      static const uint8_t ADDR = 0x0d;
+    };
+
+    struct SUBADR1 {
+      static const uint8_t ADDR = 0x0e;
+    };
+
+    struct SUBADR2 {
+      static const uint8_t ADDR = 0x0f;
+    };
+
+    struct SUBADR3 {
+      static const uint8_t ADDR = 0x10;
+    };
+
+    struct ALLCALLADR {
+      static const uint8_t ADDR = 0x11;
+    };
+
+    struct IREF {
+      static const uint8_t ADDR = 0x12;
+
+      static const uint8_t CM = 0x80; // current multiplier
+      static const uint8_t HC = 0x40; // subcurrent
+    };
+
+    struct EFLAG {
+      static const uint8_t ADDR = 0x13;
+    };
+  };
+
+  TLC59108(PinName p_sda, PinName p_scl, const uint8_t selectable_address = 0);
+  uint8_t init();
+  uint8_t reset();
+  uint8_t setLedOutputMode(const uint8_t outputMode);
+  uint8_t setBrightness(const uint8_t pwmChannel, const uint8_t dutyCycle);
+  uint8_t setBrightness(const uint8_t dutyCycle);
+  uint8_t setBrightness(const uint8_t dutyCycles[],
+                        const uint8_t startChannel = 0,
+                        const uint8_t length = NUM_CHANNELS);
+  uint8_t setGroupBrightness(const uint8_t dutyCycle);
+  int setRegister(const uint8_t reg, const uint8_t value);
+  int setRegisters(const uint8_t reg, const uint8_t values[],
+                   const uint8_t length);
 
 private:
-    const static uint8_t NUM_CHANNELS = 8;
-    uint8_t addr;
-    I2C *i2c;
+  const static uint8_t NUM_CHANNELS = 8;
+  uint8_t addr;
+  I2C *i2c;
 };
 
 #endif
